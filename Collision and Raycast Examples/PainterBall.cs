b7805df83d8071804f9045bfe87062e945358799
quizcanners <quizcanners@gmail.com>
2020-08-11 14:04:48 +0300

    asd
    more
    
    Resharper
    
    Separate Examples
    
    fixed
    
    lc
    
    Shader dependencies
    
    Organizing
    
    Shadow Data
    
    Moved Noise Texture MGMT
    
    moved some shaders
    
    Directory Name fixes
    
    Update Painter_Data_Demo_MoveToResources.asset
    
    cfg2
    
    picker
    
    scene
    
    fixes
    
    shader fix
    
    things
    
    fixes
    
    * god mode
    
    Blurred Screen
    
    SDF from Alpha
    
    cleanup
    
    pix art
    
    small change
    
    asdsad
    
    void Inspect
    
    ывыв
    
    Namespace Fixes
    
    God Mode
    
    Refucktoring
    
    asd
    
    Editor
    
    light caster
    
    IsEntered
    
    Bumped
    
    Shaders
    
    Bevel Shader stuff
    
    more stuff
    
    things
    
    Inspector rework No change MGMT
    
    isFoldout
    
    Update TutorialScene.unity
    
    Perf tex fix
    
    PP_
    
    Old Light Casters removed
    
    Using Refactory
    
    Inspector changed
    
    * OVerride
    
    Lerp
    
    PEGI_Override
    
    Versioning
    
    Moved to painter
    
    Good stuff
    
    Grand Renaming
    
    saving loading
    
    even more stuffs
    
    moved some folders
    
    data
    
    Buildable
    
    compile_local
    
    Some materials
    
    * meta
    
    stuff
    
    Moved Out Procedural UI
    
    stuff
    
    K words
    
    asd
    
    ex
    
    asd
    
    asd
    
    qwe
    
    ChangesToken
    
    TextToken
    
    TextToken
    
    asd
    
    asd
    
    asd
    
    qwe
    
    PegiLabel

---
+++ Collision and Raycast Examples/PainterBall.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using QuizCanners.Inspect;
+using QuizCanners.Utils;
+using UnityEngine;
+
+namespace PlaytimePainter.Examples { 
+
+    [ExecuteInEditMode]
+    public class PainterBall : MonoBehaviour  , IPEGI {
+        
+        public MeshRenderer rendy;
+        public Rigidbody rigid;
+        public SphereCollider _collider;
+
+		public List<PaintingCollision> paintingOn = new List<PaintingCollision>();
+        public Brush brush = new Brush();
+
+        private void TryGetPainterFrom(GameObject go) {
+
+            var target = go.GetComponent<PlaytimePainter>();
+
+            if (!target || target.TextureEditingBlocked) return;
+
+            var col = new PaintingCollision(target);
+            paintingOn.Add(col);
+            col.vector.posFrom = transform.position;
+            col.vector.firstStroke = true;
+            target.UpdateOrSetTexTarget(TexTarget.RenderTexture);
+        }
+
+        public void OnCollisionEnter(Collision collision) => TryGetPainterFrom(collision.gameObject);
+        
+        public void OnTriggerEnter(Collider enteredCollider) => TryGetPainterFrom(enteredCollider.gameObject);
+         
+        private void TryRemove(GameObject go)
+        {
+            foreach (var p in paintingOn)
+                if (p.painter.gameObject == go)
+                {
+                    paintingOn.Remove(p);
+                    return;
+                }
+        }
+
+        public void OnTriggerExit(Collider exitedCollider) => TryRemove(exitedCollider.gameObject);
+        
+        public void OnCollisionExit(Collision exitedCollider) => TryRemove(exitedCollider.gameObject);
+        
+        public void OnEnable()  {
+            brush.SetBrushType(TexTarget.RenderTexture, BrushTypes.Sphere.Inst);
+
+            if (!rendy) 
+                rendy = GetComponent<MeshRenderer>();
+
+            if (!rigid)
+                rigid = GetComponent<Rigidbody>();
+
+            if (!_collider)
+                _collider = GetComponent<SphereCollider>();
+
+            if (rendy)
+                rendy.sharedMaterial.color = brush.Color;
+
+            brush.targetIsTex2D = false;
+        }
+
+        private void Update() {
+
+            brush.brush3DRadius = transform.lossyScale.x*1.4f;
+
+			foreach (var col in paintingOn){
+				var p = col.painter;
+
+                if (!p.PaintCommand.SetBrush(brush).Is3DBrush) continue;
+
+                var v = col.vector;
+                v.posTo = transform.position;
+
+                brush.Paint(p.PaintCommand.SetStroke(v));//v, p);
+
+            }
+        }
+
+        public void Inspect()
+        {
+
+            pegi.FullWindow.DocumentationClickOpen(()=> "When colliding with other object will try to use sphere brush to paint on them." +
+                                                       "Targets need to have PlaytimePainter component", "About Painter Ball");
+     
+            if (Application.isPlaying)
+                "Painting on {0} objects".F(paintingOn.Count).PegiLabel().nl();
+
+            if (_collider.isTrigger && "Set as Rigid Collider object".PegiLabel().Click().nl())
+            {
+                _collider.isTrigger = false;
+                rigid.isKinematic = false;
+                rigid.useGravity = true;
+            }
+
+            if (!_collider.isTrigger)
+                "Set as Trigger".PegiLabel().Click().nl().OnChanged(() =>
+                    {
+                        _collider.isTrigger = true;
+                        rigid.isKinematic = true;
+                        rigid.useGravity = false;
+                    });
+
+            var size = transform.localScale.x;
+
+            if ("Size:".PegiLabel("Size of the ball", 50).edit( ref size, 0.1f, 10).nl())
+                transform.localScale = Vector3.one * size;
+
+            const string ballHint = "PaintBall_brushHint";
+
+            "Painter ball made for World Space Brushes only".PegiLabel().writeOneTimeHint(ballHint);
+
+            if ((pegi.Nested_Inspect(brush.Targets_PEGI).nl()) | (pegi.Nested_Inspect(brush.Mode_Type_PEGI).nl()))
+            {
+                if (brush.targetIsTex2D || !brush.Is3DBrush())
+                {
+                    brush.targetIsTex2D = false;
+                    brush.SetBrushType(TexTarget.RenderTexture, BrushTypes.Sphere.Inst);
+
+                    pegi.resetOneTimeHint(ballHint);
+                }
+            }
+
+            if (pegi.Nested_Inspect(brush.ColorSliders))
+                rendy.sharedMaterial.color = brush.Color;
+        }
+
+    }
+
+    public class PaintingCollision
+    {
+        public Stroke vector;
+        public PlaytimePainter painter;
+
+        public PaintingCollision(PlaytimePainter p)
+        {
+            painter = p;
+            vector = new Stroke();
+        }
+    }
+
+
+    [PEGI_Inspector_Override(typeof(PainterBall))] internal class PainterBallEditor : PEGI_Inspector_Override { }
+
+}
